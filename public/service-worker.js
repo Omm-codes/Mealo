fc7c6dc4f9b7c2ebca80b39df75dce3bb73223b1
Omm-codes <omsanjay975@gmail.com>
2025-06-29 23:00:04 +0530

    pwa

---
+++ public/service-worker.js
@@ -0,0 +1,35 @@
+// This is a basic service worker for offline support (CRA default).
+const CACHE_NAME = 'mealo-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
